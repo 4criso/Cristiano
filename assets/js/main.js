bdc623509a08caf3c2447b8de13e536d9b2a808b
4criso <127700348+4criso@users.noreply.github.com>
2023-09-24 17:11:32 -0300

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,6 @@ function updatePortfolio(profileData) {
         return `
             <li>
                 <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
-                <h2 ${project.github ? 'class="github"' : ''}>${project.name2}</h2>
                 <a href="${project.url}" target="_blank">${project.url}</a>
             </li>
         `
