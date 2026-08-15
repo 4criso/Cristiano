cdeb58443296fc918255653ad8dc9a7550053d23
4criso <4criso@hotmail.com>
2026-08-15 09:43:25 -0300

    add

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,35 +1,17 @@
 
 const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
-function closeAcordeon(acordeon) {
-    const content = acordeon.querySelector('.content')
-    content.style.maxHeight = null
-    acordeon.classList.remove('open')
-}
-
-function openAcordeon(acordeon) {
-    const content = acordeon.querySelector('.content')
-    acordeon.classList.add('open')
-    content.style.maxHeight = content.scrollHeight + 'px'
-}
-
 acordeonTriggers.forEach((trigger) => {
     trigger.addEventListener('click', () => {
         const acordeon = trigger.parentElement
         const isOpen = acordeon.classList.contains('open')
 
         document.querySelectorAll('.acordeon.open').forEach((openAcordeon) => {
-            closeAcordeon(openAcordeon)
+            openAcordeon.classList.remove('open')
         })
 
         if (!isOpen) {
-            openAcordeon(acordeon)
+            acordeon.classList.add('open')
         }
-    })
-})
-
-window.addEventListener('resize', () => {
-    document.querySelectorAll('.acordeon.open .content').forEach((content) => {
-        content.style.maxHeight = content.scrollHeight + 'px'
     })
 })
